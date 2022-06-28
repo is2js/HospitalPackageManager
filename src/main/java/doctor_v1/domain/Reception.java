c9daf70a9e7aa106ab1251e3517591818fc24b47
is2js <tingstyle1@gmail.com>
2022-06-28 18:07:38 +0900

    feat(domain/Reception): doctor가 주는 package를 받아 저장하는 기능 구현

--- src/main/java/doctor_v1/domain/Reception.java
+++ src/main/java/doctor_v1/domain/Reception.java
@@ -1,9 +1,22 @@
 package doctor_v1.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Reception {
     private final Long fee;
 
+    public List<Package> getPackages() {
+        return packages;
+    }
+
+    private List<Package> packages = new ArrayList<>();
+
     public Reception(final Long fee) {
         this.fee = fee;
+    }
+
+    public void addPackage(final Package packageItem) {
+        this.packages.add(packageItem);
     }
 }
