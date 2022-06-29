914ecd3fc31fdafddfda47587155439869f9ada2
is2js <tingstyle1@gmail.com>
2022-06-29 18:30:45 +0900

    feat(vo/CommissionRate): 커미선율에 대한 VO 생성

---
+++ src/main/java/doctor_v2/vo/CommissionRate.java
@@ -0,0 +1,17 @@
+package doctor_v2.vo;
+
+public class CommissionRate {
+    private final Double commissionRate;
+
+    public CommissionRate(final Double commissionRate) {
+
+        this.commissionRate = commissionRate;
+    }
+
+    public static CommissionRate of(final Double commissionRate) {
+        if (!(0.0 <= commissionRate  && commissionRate <= 100.0)) {
+            throw new IllegalArgumentException("[ERROR] 0~100 사이 값을 입력하세요.");
+        }
+        return new CommissionRate(commissionRate);
+    }
+}
