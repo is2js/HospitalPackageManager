b5cbbed111996f099f2975fe787448d70ff80197
is2js <tingstyle1@gmail.com>
2022-06-30 18:23:54 +0900

    feat(Reception#sellPackage): Package판매기능 구현

--- src/main/java/doctor_v2/domain/Reception.java
+++ src/main/java/doctor_v2/domain/Reception.java
@@ -6,9 +6,10 @@ import doctor_v2.vo.Count;
 import doctor_v2.vo.Money;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class Reception {
-    private final Money amount;
+    private Money amount;
 
     private Map<Doctor, CommissionRate> doctors = new HashMap<>();
 
@@ -50,9 +51,10 @@ public class Reception {
 //    }
 
     public boolean contract(final Doctor doctor, final CommissionRate commissionRate) {
-        if (doctors.containsKey(doctor)){
+        if (doctors.containsKey(doctor)) {
             return false;
         }
+
         doctors.put(doctor, commissionRate);
         return true;
     }
@@ -68,5 +70,56 @@ public class Reception {
 
     public Map<Doctor, CommissionRate> getDoctors() {
         return doctors;
+    }
+
+    public Package sellPackage(final Doctor doctor,
+                               final Specialty specialty, final Treatment treatment,
+                               final Count count) {
+        // A-1) 구매정보 검증
+        // A: 나와 계약된 doctor인가 &&
+        // B: doctor에 명시된 specialty - treament 상-하위도메인 매칭되는 구매정보인가 &&
+        // C: treamtment에 명시된 구매가능 갯수 > count로 (갯수가 모자라진 않나)
+        // -> A && B && C 해야 판매수행 -> not A || not B || not C 이면 early return NULL객체
+        if (!doctors.containsKey(doctor)
+                || !doctor.isValidMatching(specialty, treatment)
+                || !treatment.hasCount(count)) {
+            return Package.EMPTY;
+        }
+
+        // A-2) 물건검증 전 doctor에게 생성된 물건 떼오기
+        final Package packageItem = doctor.sellPackage(specialty, treatment, count);
+
+        // A-3) 떼온 물건 검증
+        if (packageItem != Package.EMPTY) {
+            // A-4) 돈 가산 (reception, doctor)
+            final Money sales = calculateFee(specialty, treatment, count);
+            final CommissionRate commissionRate = doctors.get(doctor);
+            final Money commission = sales.multiRate(commissionRate);
+            final Money salesWithNoCommission = sales.minus(commission);
+            amount = amount.plus(commission); // reception의 커미션 돈가산
+            doctor.plusAmount(salesWithNoCommission); // doctor의 나머지금액 가산
+        }
+
+        // A-4) NULL객체의 가능성을 가진 물건변수이며, 정상/NULL객체인지는 알아서 결정되므로
+        //      성공if 내부가 아니라 밖에서 return한다.
+        return packageItem;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Reception reception = (Reception) o;
+        return Objects.equals(amount, reception.amount) && Objects.equals(getDoctors(),
+            reception.getDoctors());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount, getDoctors());
     }
 }
