9754f2248762d9599e89f74a059c9fdc62cba4f2
is2js <tingstyle1@gmail.com>
2022-07-02 01:21:43 +0900

    feat(Treatment): 할인정책 판매요일을 확인하기 위해서, 판매날짜 필드 추가

--- src/main/java/doctor_v2/domain/Treatment.java
+++ src/main/java/doctor_v2/domain/Treatment.java
@@ -4,6 +4,7 @@ import doctor_v2.vo.Count;
 import doctor_v2.vo.Description;
 import doctor_v2.vo.Sequence;
 import doctor_v2.vo.Title;
+import java.time.LocalDate;
 import java.util.Objects;
 
 public class Treatment {
@@ -12,15 +13,17 @@ public class Treatment {
     private final Title title;
     private final Description description;
     private Count count;
+    private final LocalDate saleDate;
 
     public Treatment(final Sequence sequence,
                      final Title title,
                      final Description description,
-                     final Count count) {
+                     final Count count, final LocalDate saleDate) {
         this.sequence = sequence;
         this.title = title;
         this.description = description;
         this.count = count;
+        this.saleDate = saleDate;
     }
 
     public boolean hasCount(final Count count) {
@@ -34,8 +37,28 @@ public class Treatment {
         this.count = this.count.minus(count);
     }
 
+    public boolean isSequenceIn(final Sequence sequence) {
+        return this.sequence.isIn(sequence);
+    }
+
+    public Sequence getSequence() {
+        return sequence;
+    }
+
+    public Title getTitle() {
+        return title;
+    }
+
+    public Description getDescription() {
+        return description;
+    }
+
     public Count getCount() {
         return count;
+    }
+
+    public LocalDate getSaleDate() {
+        return saleDate;
     }
 
     @Override
@@ -43,16 +66,18 @@ public class Treatment {
         if (this == o) {
             return true;
         }
-        if (!(o instanceof Treatment)) {
+        if (o == null || getClass() != o.getClass()) {
             return false;
         }
         final Treatment treatment = (Treatment) o;
-        return Objects.equals(sequence, treatment.sequence) && Objects.equals(title, treatment.title)
-            && Objects.equals(description, treatment.description);
+        return Objects.equals(getSequence(), treatment.getSequence()) && Objects.equals(getTitle(),
+            treatment.getTitle()) && Objects.equals(getDescription(), treatment.getDescription())
+            && Objects.equals(getCount(), treatment.getCount()) && Objects.equals(getSaleDate(),
+            treatment.getSaleDate());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(sequence, title, description);
+        return Objects.hash(getSequence(), getTitle(), getDescription(), getCount(), getSaleDate());
     }
 }
