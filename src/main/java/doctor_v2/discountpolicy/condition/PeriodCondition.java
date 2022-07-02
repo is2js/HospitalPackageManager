39e7841d200e1158b3d186dfdbed1d1e066ca344
is2js <tingstyle1@gmail.com>
2022-07-02 23:21:47 +0900

    feat(discountpolicy/PeriodCondition): PeriodCondition 구상 메서드 및 테스트 구현

---
+++ src/main/java/doctor_v2/discountpolicy/condition/PeriodCondition.java
@@ -0,0 +1,21 @@
+package doctor_v2.discountpolicy.condition;
+
+import doctor_v2.domain.Treatment;
+import doctor_v2.vo.Count;
+import java.time.LocalDate;
+
+public class PeriodCondition implements DiscountCondition {
+
+    private long eventDays;
+    private LocalDate purchaseDate;
+
+    public PeriodCondition(final long eventDays, final LocalDate purchaseDate) {
+        this.eventDays = eventDays;
+        this.purchaseDate = purchaseDate;
+    }
+
+    @Override
+    public boolean isSatisfiedBy(final Treatment treatment, final Count count) {
+        return treatment.isInEventPeriod(eventDays, purchaseDate);
+    }
+}
